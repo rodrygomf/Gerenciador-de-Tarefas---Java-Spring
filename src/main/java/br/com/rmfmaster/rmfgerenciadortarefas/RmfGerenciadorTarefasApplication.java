14b96d0afa9144b8e89292603c3573c9d5af50ab
Rodrigo Figueiredo <rmfmaster@gmail.com>
2019-07-03 18:10:39 -0300

    chore: commit inicial

---
+++ src/main/java/br/com/rmfmaster/rmfgerenciadortarefas/RmfGerenciadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.rmfmaster.rmfgerenciadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmfGerenciadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmfGerenciadorTarefasApplication.class, args);
+	}
+
+}
